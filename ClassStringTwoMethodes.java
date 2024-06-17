be3b6902371d3c212660baef358a3a21c3654b7e
ama-player0000 <amirabolfathy884@gmail.com>
2024-06-17 19:39:02 +0330

    Class String Two Methodes. 'File => ClassStringTwoMethodes.java'

---
+++ ClassStringTwoMethodes.java
@@ -0,0 +1,11 @@
+public class ClassStringTwoMethodes {
+    public static void main(String[] args) {
+        // Use Method : charAt(index)
+        // Use Method : indexOf(char)
+        String S;
+        S = "Hello";
+        System.out.println(S);
+        System.out.println("charAt(index: 0)"+S.charAt(0)); //output : H
+        System.out.println("indexOf(char: e)"+S.indexOf("e")); //output : 1
+    }
+}
